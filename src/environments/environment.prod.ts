5acdcf8e0976e9df5010b1c2e691492e26e77ea2
hariharan21041 <hariharan2104@gmail.com>
2026-05-15 15:19:03 +0530

    Production api URL updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
     envName: 'prod',
   
     api: {
-      baseUrl: 'https://patheya-express.onrender.com/api'
+      baseUrl: 'https://patheya-prod.onrender.com'
     }
   };
