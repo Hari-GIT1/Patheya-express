65b6860af54460c425562dc92f655a5aaef1ef91
hariharan21041 <hariharan2104@gmail.com>
2026-05-15 13:08:50 +0530

    Created multiple environments

--- src/app/core/services/socket.service.ts
+++ src/app/core/services/socket.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { io, Socket } from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,9 @@ export class SocketService {
   private socket: Socket;
 
   constructor() {
-    this.socket = io('https://patheya-express.onrender.com');
+    this.socket = io(
+      environment.api.baseUrl.replace('/api', '')
+    );
   }
 
   joinOrder(orderId: string) {
---
+++ src/environments/environment.qa.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    envName: 'qa',
+  
+    api: {
+      baseUrl: 'https://patheya-express.onrender.com/api'
+    }
+  };
